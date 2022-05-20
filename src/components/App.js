ef06e2aab2cd4f98717cd731da686f0a9ced0286
lemoscaio <caio_lemos94@hotmail.com>
2022-05-20 19:00:34 -0300

    adjusts globalstyles and fonts

--- src/components/App.js
+++ src/components/App.js
@@ -4,7 +4,7 @@ import { ThemeProvider } from "styled-components"
 
 import DefaultPage from "../layouts/DefaultPage.js"
 
-import GlobalStyle from "../styles/normalize.js"
+import GlobalStyle from "../styles/globalstyle.js"
 import { theme } from "../styles/theme.js"
 import MainPage from "../pages/MainPage.js"
 
@@ -17,8 +17,6 @@ export default function App() {
                     <Route path="/" element={<DefaultPage />}>
                         <Route path="" element={<MainPage />} />
                     </Route>
-                    {/* <Route path="/sign-up" element={<SignUp />} />
-                                <Route path="/sign-in" element={<SignIn />} /> */}
                 </Routes>
             </BrowserRouter>
         </ThemeProvider>
---
+++ src/styles/globalstyle.js
@@ -0,0 +1,34 @@
+import { createGlobalStyle } from "styled-components"
+
+export const GlobalStyle = createGlobalStyle`
+
+* {
+    box-sizing: border-box;
+}
+
+html {
+  line-height: 1.15; /* 1 */
+  -webkit-text-size-adjust: 100%; /* 2 */
+}
+
+body {
+  margin: 0;
+  font-family: 'Inter', sans-serif;
+  background: #F2EFEA;
+
+  &::-webkit-scrollbar-track {
+  -webkit-box-shadow: inset 0 0 6px rgba(0, 0, 0, 0.3);
+  box-shadow: inset 0 0 6px rgba(0, 0, 0, 0.3);
+  background-color: #f5f5f5;
+  }
+  &::-webkit-scrollbar {
+  width: 10px;
+  background-color: #f5f5f5;
+  }
+  &::-webkit-scrollbar-thumb {
+  background-color: ${({ theme }) => theme.colors.scrollbarColor};
+  }
+}
+`
+
+export default GlobalStyle
