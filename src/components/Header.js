96bdf187e258565fdcdc857973da2ce02a609dd9
Hiann <hiannmanso@email.com>
2022-05-20 18:47:36 -0300

    reset css

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,15 @@
 import React from "react"
+import { Link } from "react-router-dom"
 
 export default function Header() {
-    return <div>Header</div>
+    return (
+        <header>
+            <img src={""} alt="" />
+            <div>
+                <Link to="/">Python</Link>
+                <Link to="/">JavaScript</Link>
+                <Link to="/">PHP</Link>
+            </div>
+        </header>
+    )
 }
